7cb89cca6adfea4930aef297f5c14208944c15c6
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2023-10-22 19:19:02 -0300

    versao OK

--- service-new-order/src/main/java/br/com/bicmsystems/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/bicmsystems/NewOrderMain.java
@@ -12,25 +12,17 @@ public class NewOrderMain {
 
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
 
-//            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+            var email = Math.random() + "@email.com";
+            for (int i = 0; i < 50; i++) {
 
-                var email = Math.random() + "@email.com";
-                for (int i = 0; i < 50; i++) {
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                var order = new Order(orderId, amount, email);
+                var id = new CorrelationId(NewOrderMain.class.getSimpleName());
 
-                    var orderId = UUID.randomUUID().toString();
-                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
-                    var order = new Order(orderId, amount, email);
-                    var id = new CorrelationId(NewOrderMain.class.getSimpleName());
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", id, email, order);
 
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", id, email, order);
-
-//                    var emailText = new Email("Reporting status",
-//                            "Olá " + email + ", Thank you for your order! We are processing your order!");
-//                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", id, email, emailText);
-
-                }
-
-//            }
+            }
 
         }
 
